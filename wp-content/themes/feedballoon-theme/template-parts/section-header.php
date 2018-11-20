412115a2d1f353dd05eddde19a7021e14d98d37b
sukhmeenjot <44179087+sukhmeenjot@users.noreply.github.com>
2018-11-19 17:07:18 -0800

    header part pls check

--- wp-content/themes/feedballoon-theme/template-parts/section-header.php
+++ wp-content/themes/feedballoon-theme/template-parts/section-header.php
@@ -1,11 +1,22 @@
 <div class="header">
-<<<<<<< HEAD
-  <div class="menu">
+
+  <div class="menupart">
     <ul>
       <li>ABOUT US</li>
       <li>CONTACT</li>
     </ul>
   </div>
-  <div class="content">
-
+  <div class="main1">
+    <div class="title1">
+    <img src="" alt="">
+    <h1>feedballoon</h1>
+    </div>
+    <div class="content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    </div>
+    <div class="download">
+      <p>Downloaded here</p>
+      <img src="" alt="">
+      <img src="" alt="">
+    </div>
   </div>
