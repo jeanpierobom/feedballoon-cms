af7657cc3f9bca77f33249ae098b68167b502d15
sukhmeenjot <44179087+sukhmeenjot@users.noreply.github.com>
2018-11-26 16:38:36 -0800

    update

--- wp-content/themes/feedballoon-theme/template-parts/section-header.php
+++ wp-content/themes/feedballoon-theme/template-parts/section-header.php
@@ -1,22 +1,51 @@
+<?php  ?>
+
+<style media="screen">
+
+
+  /* .header {
+    background: linear-gradient(), url(<?php  ?>);
+  } */
+</style>
+
 <div class="header">
+  <!-- <?php the_custom_header_markup(); ?> -->
 
   <div class="menupart">
     <ul>
-      <li>ABOUT US</li>
-      <li>CONTACT</li>
+      <li><a href="#">ABOUT US</a></li>
+      <li><a href="#">CONTACT</a></li>
     </ul>
   </div>
-  <div class="main1">
-    <div class="title1">
-    <img src="" alt="">
-    <h1>feedballoon</h1>
+  <div class="main">
+    <div class="title">
+    <a href="#">
+  <img src="<?php echo wp_get_attachment_image_src(28)[0];
+ ?>" alt=""> </a>
+ <h1>feedballoon</h1>
     </div>
     <div class="content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      <p>A platform designed to encourage coworkers to give, request and receive instant feedback in order to cultivate empowered, collaborative, skilled and cross-functional teams. </p>
     </div>
     <div class="download">
-      <p>Downloaded here</p>
-      <img src="" alt="">
-      <img src="" alt="">
+      <div class="part1">
+        <p>Downloaded here</p>
+
+      </div>
+      <div class="both">
+        <div class="first">
+          <a href="#">
+        <img src="<?php echo wp_get_attachment_image_src(45)[0];
+       ?>" alt=""> </a>
+        </div>
+        <div class="second">
+          <a href="#">
+        <img src="<?php echo wp_get_attachment_image_src(46)[0];
+        ?>" alt=""> </a>
+        </div>
+      </div>
+
+
+
     </div>
   </div>
