477831f1d1d4f956c11ed5173172aaef725bfff2
jeanpierobom <pierobom@gmail.com>
2018-11-29 12:54:11 -0800

    Header and variables

--- wp-content/themes/feedballoon-theme/template-parts/section-header.php
+++ wp-content/themes/feedballoon-theme/template-parts/section-header.php
@@ -6,5 +6,28 @@
   ) );
   ?>
 </nav><!-- #site-navigation -->
-<h1><?php bloginfo('name')?></h1>
-<img class="logo-image" src="<?php echo wp_get_attachment_image_src(37, 'large')[0];?>">
+
+<div class="header-container">
+  <div class="logo-container">
+    <h1 class="screen-reader-text"><?php bloginfo('name')?></h1>
+    <img class="logo-image" src="<?php echo wp_get_attachment_image_src(37, 'large')[0];?>">
+  </div>
+
+  <div class="content">
+    <p>A platform designed to encourage coworkers to give, request and receive instant feedback in order to cultivate empowered, collaborative, skilled and cross-functional teams. </p>
+  </div>
+
+  <div class="download-app">
+    <div class="label">
+      <span>Download here</span>
+    </div>
+    <div class="icons">
+      <div class="first">
+        <a href="#"><img src="<?php echo wp_get_attachment_image_src(31, 'small')[0];?>"></a>
+      </div>
+      <div class="second">
+        <a href="#"><img src="<?php echo wp_get_attachment_image_src(29, 'small')[0];?>"></a>
+      </div>
+    </div>
+  </div>
+</div>
