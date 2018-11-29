4ba5ff7422619d2d3de29b29398c35bac02bbf98
J-Caldas <joao.caldas@live.com>
2018-11-29 10:00:43 -0800

    Menu

--- wp-content/themes/feedballoon-theme/template-parts/section-footer.php
+++ wp-content/themes/feedballoon-theme/template-parts/section-footer.php
@@ -2,12 +2,16 @@ SECTION-FOOTER
 <div class="section-footer">
 
 <!-- SOCIAL MEDIA ICONS -->
-<div class="socialMedia-Icons">
-  <nav>
-    <ul>
-      <li></li>
-    </ul>
-  </nav>
+  <nav id="site-navigation" class="socialMedia-Icons">
+    <button class="menu-toggle" aria-controls="secondary-menu" aria-expanded="false"><?php esc_html_e( 'Social Media', 'feedballoon-theme' ); ?></button>
+    <?php
+    wp_nav_menu( array(
+      'theme_location' => 'menu-2',
+      'menu_id'        => 'secondary-menu',
+    ) );
+    ?>
+  </nav><!-- SOCIAL MEDIA ICONS -->
+
 </div>
 
 
