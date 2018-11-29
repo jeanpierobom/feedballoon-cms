ed0aea531d220c4602899ca457050ef4bbd3e3a8
J-Caldas <joao.caldas@live.com>
2018-11-29 15:37:28 -0800

    Footer Update

--- wp-content/themes/feedballoon-theme/template-parts/section-footer.php
+++ wp-content/themes/feedballoon-theme/template-parts/section-footer.php
@@ -1,41 +1,51 @@
 SECTION-FOOTER
 <div class="section-footer">
 
-  <div class="footer-socialMedia">
-  <h3 class="subtitle-sociaMedia">FOLLOW US</h3>
+  <div class="wrapper">
 
-    <!-- SOCIAL MEDIA ICONS -->
-    <nav id="site-navigation" class="socialMedia-Icons">
-      <?php
-      wp_nav_menu( array(
-        'theme_location' => 'menu-2',
-        'menu_id'        => 'secondary-menu',
-      ) );
-      ?>
-    </nav><!-- SOCIAL MEDIA ICONS -->
-  </div>
-
-  <div class="footer-stores">
-    <span class="downloadApp">DOWNLOAD</span>
-
-    <div class="stores-logos-container">
-      <img class="logo-appStore logo-stores" src="<?php echo wp_get_attachment_image_src(29, 'large')[0];?>">
-      <img class="ogo-playStore logo-stores" src="<?php echo wp_get_attachment_image_src(31, 'large')[0];?>">
+    <div class="balloonIcon-box">
+      <img class="balloonIcon" src="<?php echo wp_get_attachment_image_src(30, 'large')[0];?>">
     </div>
 
-    <div class="footer-info">
-      <img class="logo-footer" src="<?php echo wp_get_attachment_image_src(36, 'large')[0];?>">
-    </div>
+    <div class="fallback-wrapper">
 
-    <div class="footer-rodape">
+      <div class="footer-socialMedia">
+      <h3 class="subtitle-sociaMedia">FOLLOW US</h3>
+
+        <!-- SOCIAL MEDIA ICONS -->
+        <nav id="site-navigation" class="socialMedia-Icons">
+          <?php
+          wp_nav_menu( array(
+            'theme_location' => 'menu-2',
+            'menu_id'        => 'secondary-menu',
+          ) );
+          ?>
+        </nav><!-- SOCIAL MEDIA ICONS -->
+      </div>
+
+      <div class="footer-stores">
+        <span class="downloadApp">DOWNLOAD</span>
+
+        <div class="stores-logos-container">
+          <img class="logo-appStore logo-stores" src="<?php echo wp_get_attachment_image_src(29, 'large')[0];?>">
+          <img class="ogo-playStore logo-stores" src="<?php echo wp_get_attachment_image_src(31, 'large')[0];?>">
+        </div>
+
+      </div>
+
+      <div class="footer-info">
+        <img class="logo-footer" src="<?php echo wp_get_attachment_image_src(36, 'large')[0];?>">
+      </div>
+
+    </div> <!-- fallbaclk wrapper - END -->
+
+
+
+</div>  <!-- Wrapper - END -->
+
+  <div class="footer-rodape">
     <p class="copyright rodape-text">Copyright © 2018. All rights reserved.</p>
     <p class="footer-location rodape-text">Vancouver, Canada</p>
-
-    </div>
-
   </div>
-
-
-
 
 </div> <!-- section-footer - END -->
