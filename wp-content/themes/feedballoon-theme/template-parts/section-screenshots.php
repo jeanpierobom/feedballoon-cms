f142912132671889dafa36cb65b106e04cc301f4
J-Caldas <joao.caldas@live.com>
2018-11-29 18:18:58 -0800

    Footer and Feedback Section

--- wp-content/themes/feedballoon-theme/template-parts/section-screenshots.php
+++ wp-content/themes/feedballoon-theme/template-parts/section-screenshots.php
@@ -1,1 +1,51 @@
-SECTION-SCREENSHOTS
+<div class="section-feedback">
+  <h1>Feedbacks</h1>
+
+
+
+
+  <div class="awesomeBox">
+    <div class="border">
+      <div class="awesomeInternalBox">
+
+        <span class="initials">JC</span>
+
+        <div class="userInfo clearfix">
+          <span class="name">Joao Caldas</span>
+          <span class="titleCompany">Account Manager @FeedBalloon Co.</span>
+          <span class="date">Feb 14, 2018 - 11:00am</span>
+        </div>
+
+        <div class="bar">
+          <span class="type">AWESOME</span>
+          <span class="reply"><i class="fas fa-reply"></i></span>
+          <span class="favorite"><i class="far fa-bookmark"></i></span>
+        </div>
+
+        <div class="bottomInfo">
+          <p>
+          Hey John, <span class="break">I really liked your presentation today. It was AWESOME! Keep up with the good structure in your slides.</span>
+          <span class="break">Great Job!</span>
+        <span class="break"> Sarah Inman
+          Marketing Manager @FeedBaloon Co.</span>
+        </p>
+        </div>
+
+
+      </div>
+    </div>
+  </div>
+
+
+  <div class="reviseBox">
+    <div class="border">
+
+    </div>
+  </div>
+
+
+
+
+
+
+</div> <!-- section-feedback - END -->
