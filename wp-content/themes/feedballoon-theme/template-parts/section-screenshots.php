970a77e6530f4fdbef69afabf6a5c9cf0b045c6a
J-Caldas <joao.caldas@live.com>
2018-12-01 13:01:43 -0800

    FEEDBACK IN PROGRESS

--- wp-content/themes/feedballoon-theme/template-parts/section-screenshots.php
+++ wp-content/themes/feedballoon-theme/template-parts/section-screenshots.php
@@ -1,7 +1,9 @@
 <div class="section-feedback">
-  <h1>Feedbacks</h1>
+  <h1>Feedback</h1>
 
 
+
+<div class="wrapper">
 
 
   <div class="awesomeBox">
@@ -11,7 +13,7 @@
         <span class="initials">JC</span>
 
         <div class="userInfo clearfix">
-          <span class="name">Joao Caldas</span>
+          <span class="name">John Mosby</span>
           <span class="titleCompany">Account Manager @FeedBalloon Co.</span>
           <span class="date">Feb 14, 2018 - 11:00am</span>
         </div>
@@ -24,28 +26,54 @@
 
         <div class="bottomInfo">
           <p>
-          Hey John, <span class="break">I really liked your presentation today. It was AWESOME! Keep up with the good structure in your slides.</span>
+          Hey John, <span class="break1">I really liked your presentation today. It was AWESOME! Keep up with the good structure in your slides.</span>
           <span class="break">Great Job!</span>
-        <span class="break"> Sarah Inman
-          Marketing Manager @FeedBaloon Co.</span>
+        <span class="break">Sarah Inman</span>
+          <span class="break">Marketing Manager @FeedBaloon Co.</span>
         </p>
         </div>
 
 
       </div>
     </div>
-  </div>
+  </div> <!-- END AWESOME BOX -->
 
 
   <div class="reviseBox">
     <div class="border">
+      <div class="awesomeInternalBox">
 
+        <span class="initials">TC</span>
+
+        <div class="userInfo clearfix">
+          <span class="name">Ted Cilli</span>
+          <span class="titleCompany">Marketing Analyst @FeedBalloon Co.</span>
+          <span class="date">March 28, 2018 - 09:30am</span>
+        </div>
+
+        <div class="bar">
+          <span class="type">REVISE</span>
+          <span class="reply"><i class="fas fa-reply"></i></span>
+          <span class="favorite"><i class="far fa-bookmark"></i></span>
+        </div>
+
+        <div class="bottomInfo">
+          <p>
+           Ted, <span class="break1">I missed some sales information in your final report. I suggest you to revise the pipeline and closed orders data.</span>
+          <span class="break">Take care!</span>
+        <span class="break">Robert Than</span>
+          <span class="break">Commercial Leader @FeedBaloon Co.</span>
+        </p>
+        </div>
+
+
+      </div>
     </div>
-  </div>
+  </div> <!-- END BOX -->
 
 
 
 
 
-
+</div> <!-- wrapper - END -->
 </div> <!-- section-feedback - END -->
